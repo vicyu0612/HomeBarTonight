d08ba0c57903b90d76c94934d310006b23827b60
Vic Yu <vic.yu@volumental.com>
2026-01-05 08:47:04 +0800

    feat: Initialize Capacitor iOS project with Xcode configuration, app assets, and viewport meta tag update.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vic.homebartonight',
+  appName: 'HomeBarTonight',
+  webDir: 'dist'
+};
+
+export default config;
